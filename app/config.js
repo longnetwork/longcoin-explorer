56d6584d68169725c56baa4884beb7c5d2a1fcbf
SteepPepper <steephairy@gmail.com>
2021-08-06 14:39:26 +0300

    update links

--- app/config.js
+++ app/config.js
@@ -10,13 +10,13 @@ var currentCoin = process.env.BTCEXP_COIN || "LONG";
 var rpcCred = credentials.rpc;
 
 if (rpcCred.cookie && !rpcCred.username && !rpcCred.password && fs.existsSync(rpcCred.cookie)) {
-	console.log(`Loading RPC cookie file: ${rpcCred.cookie}`);
+    console.log(`Loading RPC cookie file: ${rpcCred.cookie}`);
 
-	[ rpcCred.username, rpcCred.password ] = fs.readFileSync(rpcCred.cookie).toString().split(':', 2);
+    [ rpcCred.username, rpcCred.password ] = fs.readFileSync(rpcCred.cookie).toString().split(':', 2);
 
-	if (!rpcCred.password) {
-		throw new Error(`Cookie file ${rpcCred.cookie} in unexpected format`);
-	}
+    if (!rpcCred.password) {
+        throw new Error(`Cookie file ${rpcCred.cookie} in unexpected format`);
+    }
 }
 
 var cookieSecret = process.env.BTCEXP_COOKIE_SECRET
@@ -28,105 +28,105 @@ var cookieSecret = process.env.BTCEXP_COOKIE_SECRET
 var electrumXServerUriStrings = (process.env.BTCEXP_ELECTRUMX_SERVERS || "").split(',').filter(Boolean);
 var electrumXServers = [];
 for (var i = 0; i < electrumXServerUriStrings.length; i++) {
-	var uri = url.parse(electrumXServerUriStrings[i]);
-        
+    var uri = url.parse(electrumXServerUriStrings[i]);
+
         if (uri.protocol) electrumXServers.push({protocol:uri.protocol.substring(0, uri.protocol.length - 1), host:uri.hostname, port:parseInt(uri.port)});
         else { global.electrumStandalone=true; electrumXServers.push({protocol:"standalone", host:"standalone", port:""}); }
 }
 
 ["BTCEXP_DEMO", "BTCEXP_PRIVACY_MODE", "BTCEXP_NO_INMEMORY_RPC_CACHE"].forEach(function(item) {
-	if (process.env[item] === undefined) {
-		process.env[item] = "false";
-	}
+    if (process.env[item] === undefined) {
+        process.env[item] = "false";
+    }
 });
 
 ["BTCEXP_NO_RATES", "BTCEXP_UI_SHOW_TOOLS_SUBHEADER"].forEach(function(item) {
-	if (process.env[item] === undefined) {
-		process.env[item] = "true";
-	}
+    if (process.env[item] === undefined) {
+        process.env[item] = "true";
+    }
 });
 
 module.exports = {
-	coin: currentCoin,
+    coin: currentCoin,
 
-	cookieSecret: cookieSecret,
+    cookieSecret: cookieSecret,
 
-	privacyMode: (process.env.BTCEXP_PRIVACY_MODE.toLowerCase() == "true"),
-	demoSite: (process.env.BTCEXP_DEMO.toLowerCase() == "true"),
-	queryExchangeRates: (process.env.BTCEXP_NO_RATES.toLowerCase() != "true"),
-	noInmemoryRpcCache: (process.env.BTCEXP_NO_INMEMORY_RPC_CACHE.toLowerCase() == "true"),
+    privacyMode: (process.env.BTCEXP_PRIVACY_MODE.toLowerCase() == "true"),
+    demoSite: (process.env.BTCEXP_DEMO.toLowerCase() == "true"),
+    queryExchangeRates: (process.env.BTCEXP_NO_RATES.toLowerCase() != "true"),
+    noInmemoryRpcCache: (process.env.BTCEXP_NO_INMEMORY_RPC_CACHE.toLowerCase() == "true"),
 
-	rpcConcurrency: (process.env.BTCEXP_RPC_CONCURRENCY || 10),
+    rpcConcurrency: (process.env.BTCEXP_RPC_CONCURRENCY || 10),
 
-	rpcBlacklist:
-	  process.env.BTCEXP_RPC_ALLOWALL  ? []
-	: process.env.BTCEXP_RPC_BLACKLIST ? process.env.BTCEXP_RPC_BLACKLIST.split(',').filter(Boolean)
-	: [
-		"addnode",
-		"backupwallet",
-		"bumpfee",
-		"clearbanned",
-		"createmultisig",
+    rpcBlacklist:
+      process.env.BTCEXP_RPC_ALLOWALL  ? []
+    : process.env.BTCEXP_RPC_BLACKLIST ? process.env.BTCEXP_RPC_BLACKLIST.split(',').filter(Boolean)
+    : [
+        "addnode",
+        "backupwallet",
+        "bumpfee",
+        "clearbanned",
+        "createmultisig",
         "getaddednodeinfo",
-		"disconnectnode",
-		"dumpprivkey",
-		"dumpwallet",
-		"encryptwallet",
-		"generate",
+        "disconnectnode",
+        "dumpprivkey",
+        "dumpwallet",
+        "encryptwallet",
+        "generate",
         "setgenerate",
-		"generatetoaddress",
-		"getaccountaddrss",
-		"getaddressesbyaccount",
-		"getbalance",
-		"getnewaddress",
-		"getrawchangeaddress",
-		"getreceivedbyaccount",
-		"getreceivedbyaddress",
-		"gettransaction",
-		"getunconfirmedbalance",
-		"getwalletinfo",
-		"importaddress",
-		"importmulti",
-		"importprivkey",
-		"importprunedfunds",
-		"importpubkey",
-		"importwallet",
-		"keypoolrefill",
-		"listaccounts",
-		"listaddressgroupings",
-		"listlockunspent",
-		"listreceivedbyaccount",
-		"listreceivedbyaddress",
-		"listsinceblock",
-		"listtransactions",
-		"listunspent",
-		"listwallets",
-		"lockunspent",
-		"logging",
-		"move",
-		"preciousblock",
-		"pruneblockchain",
-		"removeprunedfunds",
-		"rescanblockchain",
-		"savemempool",
-		"sendfrom",
-		"sendmany",
-		"sendtoaddress",
-		"sendrawtransaction",
+        "generatetoaddress",
+        "getaccountaddrss",
+        "getaddressesbyaccount",
+        "getbalance",
+        "getnewaddress",
+        "getrawchangeaddress",
+        "getreceivedbyaccount",
+        "getreceivedbyaddress",
+        "gettransaction",
+        "getunconfirmedbalance",
+        "getwalletinfo",
+        "importaddress",
+        "importmulti",
+        "importprivkey",
+        "importprunedfunds",
+        "importpubkey",
+        "importwallet",
+        "keypoolrefill",
+        "listaccounts",
+        "listaddressgroupings",
+        "listlockunspent",
+        "listreceivedbyaccount",
+        "listreceivedbyaddress",
+        "listsinceblock",
+        "listtransactions",
+        "listunspent",
+        "listwallets",
+        "lockunspent",
+        "logging",
+        "move",
+        "preciousblock",
+        "pruneblockchain",
+        "removeprunedfunds",
+        "rescanblockchain",
+        "savemempool",
+        "sendfrom",
+        "sendmany",
+        "sendtoaddress",
+        "sendrawtransaction",
         "fundrawtransaction",
-		"setaccount",
-		"setban",
-		"setnetworkactive",
-		"signmessage",
-		"signmessagewithprivatekey",
-		"signrawtransaction",
-		"stop",
-		"submitblock",
+        "setaccount",
+        "setban",
+        "setnetworkactive",
+        "signmessage",
+        "signmessagewithprivatekey",
+        "signrawtransaction",
+        "stop",
+        "submitblock",
         "prioritisetransaction",
-		"verifychain",
-		"walletlock",
-		"walletpassphrase",
-		"walletpassphrasechange",
+        "verifychain",
+        "walletlock",
+        "walletpassphrase",
+        "walletpassphrasechange",
         "abandontransaction",
         "addmultisigaddress",
         "dumppubkey",
@@ -142,73 +142,76 @@ module.exports = {
         "reconsiderblock",
         "setmocktime",
         "resendwallettransactions"
-	],
+    ],
 
-	electrumXServers:electrumXServers,
+    electrumXServers:electrumXServers,
 
-	redisUrl:process.env.BTCEXP_REDIS_URL,
+    redisUrl:process.env.BTCEXP_REDIS_URL,
 
-	site: {
-		blockTxPageSize:20,
-		addressTxPageSize:20,
+    site: {
+        blockTxPageSize:20,
+        addressTxPageSize:20,
         addressTxLimit:1000,  // TODO FIXME nedded pagination for start stop in command getAddressTxids
-		txMaxInput:500,       // TODO FIXME function getRawTransactionsWithInputs(txids, maxInputs=-1)
-		browseBlocksPageSize:20,
-		addressPage:{
-			txOutputMaxDefaultDisplay:10
-		},
-		header:{
-			showToolsSubheader:(process.env.BTCEXP_UI_SHOW_TOOLS_SUBHEADER == "true"),
-			dropdowns:[
-				{
-					title:"Networks",
-					links:[
-						//{name: "Bitcoin", url:"https://btc.horizontalsystems.xyz", imgUrl:"/img/logo/btc.svg"},
-						//{name: "Bitcoin (Testnet)", url:"http://btc-testnet.horizontalsystems.xyz", imgUrl:"/img/logo/btc.svg"},
-						//{name: "Bitcoin Cash", url:"https://bch.horizontalsystems.xyz", imgUrl:"/img/logo/bch.svg"},
-						//{name: "Bitcoin Cash (Testnet)", url:"http://bch-testnet.horizontalsystems.xyz", imgUrl:"/img/logo/bch.svg"},
-						//{name: "Ethereum", url:"https://eth.horizontalsystems.xyz", imgUrl:"/img/logo/eth.svg"},
-                                                {name: "Long Wallet", url:"https://longnetwork.github.io/", imgUrl:"/img/logo/long256.png"},
-                                                {name: "Long Exchange", url:"https://altmarkets.io/", imgUrl:"/img/logo/altmarkets.jpg"},
+        txMaxInput:500,       // TODO FIXME function getRawTransactionsWithInputs(txids, maxInputs=-1)
+        browseBlocksPageSize:20,
+        addressPage:{
+            txOutputMaxDefaultDisplay:10
+        },
+        header:{
+            showToolsSubheader:(process.env.BTCEXP_UI_SHOW_TOOLS_SUBHEADER == "true"),
+            dropdowns:[
+                {
+                    title:"Networks",
+                    links:[
+                        //{name: "Bitcoin", url:"https://btc.horizontalsystems.xyz", imgUrl:"/img/logo/btc.svg"},
+                        //{name: "Bitcoin (Testnet)", url:"http://btc-testnet.horizontalsystems.xyz", imgUrl:"/img/logo/btc.svg"},
+                        //{name: "Bitcoin Cash", url:"https://bch.horizontalsystems.xyz", imgUrl:"/img/logo/bch.svg"},
+                        //{name: "Bitcoin Cash (Testnet)", url:"http://bch-testnet.horizontalsystems.xyz", imgUrl:"/img/logo/bch.svg"},
+                        //{name: "Ethereum", url:"https://eth.horizontalsystems.xyz", imgUrl:"/img/logo/eth.svg"},
+                                                {name: "Long Wallet", url:"https://longnetwork.github.io/downloads.html", imgUrl:"/img/logo/long256.png"},
+                                                {name: "Long Faucet", url:"https://longdice.crypton.cf/", imgUrl:"/img/logo/dice.jpg"},
+                                                {name: "Long Faucet", url:"https://longjack.crypton.cf/", imgUrl:"/img/logo/26.png"},
+                                                {name: "Long Exchange", url:"https://whitebit.com/referral/f0385268-ab4f-4503-96c0-722dbea95c6d", imgUrl:"/img/logo/wb.png"},
                                                 {name: "Mining Pool", url:"https://longnomp.crypton.cf", imgUrl:"/img/logo/long256.png"},
                                                 //{name: "Mining Pool 2", url:"https://mnshare.info/", imgUrl:"https://mnshare.info/img/mnshare-300-300-pr.png"},
-                                                {name: "Long Community EN", url:"https://bitcointalk.org/index.php?topic=5235729", imgUrl:"/img/logo/bitcointalk.jpg"},                                                  
-                                                {name: "Long Community RU", url:"https://forum.bits.media/index.php?/topic/163664-long-network-%D0%B4%D0%B5%D1%86%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%81%D0%BE%D1%86-%D1%81%D0%B5%D1%82%D1%8C/", imgUrl:"/img/logo/bitsmedia.jpg"}
-					]
-				}
-			]
-		}
-	},
+                                                {name: "Long Community EN", url:"https://bitcointalk.org/index.php?topic=5235729", imgUrl:"/img/logo/bitcointalk.jpg"},
+                                                {name: "Long Community RU", url:"https://forum.bits.media/index.php?/topic/163664-long-network-%D0%B4%D0%B5%D1%86%D0%B5%D0%BD%D1%82%D1%80%D0%B0%D0%BB%D0%B8%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D1%81%D0%BE%D1%86-%D1%81%D0%B5%D1%82%D1%8C/", imgUrl:"/img/logo/bitsmedia.jpg"},
+                                                {name: "Long Support", url:"https://discord.gg/5y2PgmJ", imgUrl:"/img/logo/discords.png"}
+                    ]
+                }
+            ]
+        }
+    },
 
-	credentials: credentials,
+    credentials: credentials,
 
-	siteTools:[
-		{name:"Node Status", url:"/node-status", desc:"Summary of this node: version, network, uptime, etc.", fontawesome:"fas fa-broadcast-tower"},
-		{name:"Peers", url:"/peers", desc:"Detailed info about the peers connected to this node.", fontawesome:"fas fa-sitemap"},
+    siteTools:[
+        {name:"Node Status", url:"/node-status", desc:"Summary of this node: version, network, uptime, etc.", fontawesome:"fas fa-broadcast-tower"},
+        {name:"Peers", url:"/peers", desc:"Detailed info about the peers connected to this node.", fontawesome:"fas fa-sitemap"},
 
-		{name:"Browse Blocks", url:"/blocks", desc:"Browse all blocks in the blockchain.", fontawesome:"fas fa-cubes"},
-		//{name:"Transaction Stats", url:"/tx-stats", desc:"See graphs of total transaction volume and transaction rates.", fontawesome:"fas fa-chart-bar"},
+        {name:"Browse Blocks", url:"/blocks", desc:"Browse all blocks in the blockchain.", fontawesome:"fas fa-cubes"},
+        //{name:"Transaction Stats", url:"/tx-stats", desc:"See graphs of total transaction volume and transaction rates.", fontawesome:"fas fa-chart-bar"},
 
-		//{name:"Mempool Summary", url:"/mempool-summary", desc:"Detailed summary of the current mempool for this node.", fontawesome:"fas fa-clipboard-list"},
-		{name:"Unconfirmed Transactions", url:"/unconfirmed-tx", desc:"Browse unconfirmed/pending transactions.", fontawesome:"fas fa-unlock-alt"},
+        //{name:"Mempool Summary", url:"/mempool-summary", desc:"Detailed summary of the current mempool for this node.", fontawesome:"fas fa-clipboard-list"},
+        {name:"Unconfirmed Transactions", url:"/unconfirmed-tx", desc:"Browse unconfirmed/pending transactions.", fontawesome:"fas fa-unlock-alt"},
 
-		//{name:"RPC Browser", url:"/rpc-browser", desc:"Browse the RPC functionality of this node. See docs and execute commands.", fontawesome:"fas fa-book"},
-		//{name:"RPC Terminal", url:"/rpc-terminal", desc:"Directly execute RPCs against this node.", fontawesome:"fas fa-terminal"},
+        //{name:"RPC Browser", url:"/rpc-browser", desc:"Browse the RPC functionality of this node. See docs and execute commands.", fontawesome:"fas fa-book"},
+        //{name:"RPC Terminal", url:"/rpc-terminal", desc:"Directly execute RPCs against this node.", fontawesome:"fas fa-terminal"},
 
-		// {name:(coins[currentCoin].name + " Fun"), url:"/fun", desc:"See fun/interesting historical blockchain data.", fontawesome:"fas fa-certificate"}
-	],
+        // {name:(coins[currentCoin].name + " Fun"), url:"/fun", desc:"See fun/interesting historical blockchain data.", fontawesome:"fas fa-certificate"}
+    ],
 
-	donations:{
-		/*addresses:{
-			coins:["BTC"],
-			sites:{"BTC":"https://btc.chaintools.io"},
+    donations:{
+        /*addresses:{
+            coins:["BTC"],
+            sites:{"BTC":"https://btc.chaintools.io"},
 
-			"BTC":{address:"1NbmV7xzXWvnPkYDfx4Vh7hk3LW5237RKR"}
-		},
-		btcpayserver:{
-			host:"https://btcpay.chaintools.io",
-			storeId:"DUUExHMvKNAFukrJZHCShMhwZvfPq87QnkUhvE6h5kh2",
-			notifyEmail:"chaintools.io@gmail.com"
-		}*/
-	}
+            "BTC":{address:"1NbmV7xzXWvnPkYDfx4Vh7hk3LW5237RKR"}
+        },
+        btcpayserver:{
+            host:"https://btcpay.chaintools.io",
+            storeId:"DUUExHMvKNAFukrJZHCShMhwZvfPq87QnkUhvE6h5kh2",
+            notifyEmail:"chaintools.io@gmail.com"
+        }*/
+    }
 };
