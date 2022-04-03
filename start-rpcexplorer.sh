c7f671da830a6d4a1423598535ba33519298ba7a
SteepPepper <steephairy@gmail.com>
2022-04-03 18:07:01 +0300

    uipdate start script

--- start-rpcexplorer.sh
+++ start-rpcexplorer.sh
@@ -16,7 +16,7 @@ export BTCEXP_OLD_SPACE_MAX_SIZE=384
 forever start --id "rpcexplorer" \
 -a -l /var/log/rpcexplorer.log \
 --minUptime 10000 --spinSleepTime 10000 \
--c "n use 9.11.2" ./bin/cli.js --max-mem 384 -i 127.0.0.1 -p 8080 -C LONG -H 127.0.0.1 -P 8878 -u user -w password -E standalone
+-c "n use 9.11.2" --max-old-space-size=$BTCEXP_OLD_SPACE_MAX_SIZE ./bin/cli.js --max-mem 384 -i 127.0.0.1 -p 8080 -C LONG -H 127.0.0.1 -P 8878 -u user -w password -E standalone
 
 
 # forever list
